8363d99b5aff5d09826f0836f2ef88056753fc10
Mark Seemann <mark@ploeh.dk>
2019-08-19 11:17:24 +0200

    Add empty web project

---
+++ BookingApi/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Copyright (c) Mark Seemann 2019 all rights reserved
+ * Permission is hereby granted to share this code for educational purposes
+ * only, under the condition that this header remains intact. */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Ploeh.Samples.BookingApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ BookingApi/Startup.cs
@@ -0,0 +1,34 @@
+﻿/* Copyright (c) Mark Seemann 2019 all rights reserved
+ * Permission is hereby granted to share this code for educational purposes
+ * only, under the condition that this header remains intact. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ploeh.Samples.BookingApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
