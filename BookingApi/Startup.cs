09fa108f56338f9111f4e549af7dda79ff6fdefc
Mark Seemann <mark@ploeh.dk>
2019-08-19 16:27:57 +0200

    Move capacity value to configuration file

--- BookingApi/Startup.cs
+++ BookingApi/Startup.cs
@@ -9,17 +9,27 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ploeh.Samples.BookingApi
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            var capacity = Configuration.GetValue<int>("Capacity");
             services.AddSingleton<IControllerActivator>(
-                new CompositionRoot(10));
+                new CompositionRoot(capacity));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
