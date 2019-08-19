e1af318a0f56c57f6ddc494f5c054bf6b0922906
Mark Seemann <mark@ploeh.dk>
2019-08-19 11:53:51 +0200

    Add domain model

---
+++ BookingApi/MaîtreD.cs
@@ -0,0 +1,18 @@
+﻿/* Copyright (c) Mark Seemann 2019 all rights reserved
+ * Permission is hereby granted to share this code for educational purposes
+ * only, under the condition that this header remains intact. */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ploeh.Samples.BookingApi
+{
+    public class MaîtreD : IMaîtreD
+    {
+        public int? TryAccept(Reservation reservation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BookingApi/Reservation.cs
@@ -0,0 +1,15 @@
+﻿/* Copyright (c) Mark Seemann 2019 all rights reserved
+ * Permission is hereby granted to share this code for educational purposes
+ * only, under the condition that this header remains intact. */
+using System;
+
+namespace Ploeh.Samples.BookingApi
+{
+    public class Reservation
+    {
+        public DateTime Date { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
