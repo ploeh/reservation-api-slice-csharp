ploeh/restaurant-reservation-exercises

Add SqlReservationsRepository
To keep this example code base simple, there's no tests of this
implementation. One could hand-wave the ommission away by calling it a
Humble Object (see Meszaros 2007), but it'd be more appropriate to cover
this with some integration tests. I've plenty of examples of that in
other places, so will not include that here, as it adds complexity.

BookingApi/BookingDbSchema.sql
@@ -1,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Reservations] (
+    [Id]         INT           NOT NULL IDENTITY,
+    [Date]       DATETIME      NOT NULL,
+    [Name]       NVARCHAR (50) NOT NULL,
+    [Email]      NVARCHAR (50) NOT NULL,
+    [Quantity]   INT           NOT NULL
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+)

ploeh/reservation-api-slice-csharp

BookingApi/BookingDbSchema.sql
@@ -1,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Reservations] (
+    [Id]         INT           NOT NULL IDENTITY,
+    [Date]       DATETIME2     NOT NULL,
+    [Name]       NVARCHAR (50) NOT NULL,
+    [Email]      NVARCHAR (50) NOT NULL,
+    [Quantity]   INT           NOT NULL
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+)
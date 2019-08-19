13520f0d3e22374ce9c14ff2bfac0140f0be0efe
Mark Seemann <mark@ploeh.dk>
2019-08-19 16:55:27 +0200

    Add SqlReservationsRepository
    To keep this example code base simple, there's no tests of this
    implementation. One could hand-wave the ommission away by calling it a
    Humble Object (see Meszaros 2007), but it'd be more appropriate to cover
    this with some integration tests. I've plenty of examples of that in
    other places, so will not include that here, as it adds complexity.

---
+++ BookingApi/BookingDbSchema.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Reservations] (
+    [Id]         INT           NOT NULL IDENTITY,
+    [Date]       DATETIME2     NOT NULL,
+    [Name]       NVARCHAR (50) NOT NULL,
+    [Email]      NVARCHAR (50) NOT NULL,
+    [Quantity]   INT           NOT NULL
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+)
---
+++ BookingApi/SqlReservationsRepository.cs
@@ -0,0 +1,90 @@
+﻿/* Copyright (c) Mark Seemann 2019 all rights reserved
+ * Permission is hereby granted to share this code for educational purposes
+ * only, under the condition that this header remains intact. */
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ploeh.Samples.BookingApi
+{
+    public class SqlReservationsRepository : IReservationsRepository
+    {
+        public SqlReservationsRepository(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public string ConnectionString { get; }
+
+        public IEnumerable<Reservation> ReadReservations(DateTime date)
+        {
+            return ReadReservations(
+                date.Date,
+                date.Date.AddDays(1).AddTicks(-1));
+        }
+
+        private Reservation[] ReadReservations(DateTime min, DateTime max)
+        {
+            var result = new List<Reservation>();
+
+            using (var conn = new SqlConnection(ConnectionString))
+            using (var cmd = new SqlCommand(readByRangeSql, conn))
+            {
+                cmd.Parameters.Add(new SqlParameter("@MinDate", min));
+                cmd.Parameters.Add(new SqlParameter("@MaxDate", max));
+
+                conn.Open();
+                using (var rdr = cmd.ExecuteReader())
+                {
+                    while (rdr.Read())
+                        result.Add(
+                            new Reservation
+                            {
+                                Date = (DateTime) rdr["Date"],
+                                Name = (string) rdr["Name"],
+                                Email = (string) rdr["Email"],
+                                Quantity = (int) rdr["Quantity"]
+                            });
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        private const string readByRangeSql = @"
+            SELECT [Date], [Name], [Email], [Quantity]
+            FROM [dbo].[Reservations]
+            WHERE YEAR(@MinDate) <= YEAR([Date])
+            AND MONTH(@MinDate) <= MONTH([Date])
+            AND DAY(@MinDate) <= DAY([Date])
+            AND YEAR([Date]) <= YEAR(@MaxDate)
+            AND MONTH([Date]) <= MONTH(@MaxDate)
+            AND DAY([Date]) <= DAY(@MaxDate)";
+
+        public int Create(Reservation reservation)
+        {
+            using (var conn = new SqlConnection(ConnectionString))
+            using (var cmd = new SqlCommand(createReservationSql, conn))
+            {
+                cmd.Parameters.Add(
+                    new SqlParameter("@Date", reservation.Date));
+                cmd.Parameters.Add(
+                    new SqlParameter("@Name", reservation.Name));
+                cmd.Parameters.Add(
+                    new SqlParameter("@Email", reservation.Email));
+                cmd.Parameters.Add(
+                    new SqlParameter("@Quantity", reservation.Quantity));
+
+                conn.Open();
+                return (int)cmd.ExecuteScalar();
+            }
+        }
+
+        private const string createReservationSql = @"
+            INSERT INTO [dbo].[Reservations] ([Date], [Name], [Email], [Quantity])
+            OUTPUT INSERTED.Id
+            VALUES (@Date, @Name, @Email, @Quantity)";
+    }
+}
