ploeh/restaurant-reservation-exercises

Add GUID to reservation
This is the first step in a series of refactorings that will address the
Command-Query-Separation violation of both IReservationsRepository and
IMaîtreD.

BookingApi/BookingDbSchema.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE [dbo].[Reservations] (
-    [Id]         INT           NOT NULL IDENTITY,
-    [Date]       DATETIME      NOT NULL,
-    [Name]       NVARCHAR (50) NOT NULL,
-    [Email]      NVARCHAR (50) NOT NULL,
-    [Quantity]   INT           NOT NULL
+    [Id]         INT                NOT NULL IDENTITY,
+	[Guid]       UNIQUEIDENTIFIER   NOT NULL UNIQUE,
+    [Date]       DATETIME           NOT NULL,
+    [Name]       NVARCHAR (50)      NOT NULL,
+    [Email]      NVARCHAR (50)      NOT NULL,
+    [Quantity]   INT                NOT NULL
     PRIMARY KEY CLUSTERED ([Id] ASC)
 )

ploeh/reservation-api-slice-csharp

BookingApi/BookingDbSchema.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE [dbo].[Reservations] (
-    [Id]         INT           NOT NULL IDENTITY,
-    [Date]       DATETIME2     NOT NULL,
-    [Name]       NVARCHAR (50) NOT NULL,
-    [Email]      NVARCHAR (50) NOT NULL,
-    [Quantity]   INT           NOT NULL
+    [Id]         INT                NOT NULL IDENTITY,
+    [Guid]       UNIQUEIDENTIFIER   NOT NULL UNIQUE,
+    [Date]       DATETIME2          NOT NULL,
+    [Name]       NVARCHAR (50)      NOT NULL,
+    [Email]      NVARCHAR (50)      NOT NULL,
+    [Quantity]   INT                NOT NULL
     PRIMARY KEY CLUSTERED ([Id] ASC)
 )